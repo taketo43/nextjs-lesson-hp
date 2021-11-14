24127281fe219f7c1218fc40acd4d4681f394d11
taketo43 <take10.43.aki@gmail.com>
2021-11-14 16:24:43 +0900

    changed to function

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout"
 import Image from "next/image";
 
-const Contact = () => {
+export default function Contact() {
     return (
         <Layout title="Contect">
             <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
@@ -80,5 +80,3 @@ const Contact = () => {
         </Layout>
     )
 }
-
-export default Contact
